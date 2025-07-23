f84232ca93bcc52b27dc0a39e83cdb4364047db9
Evgenia Bullis <bulish2015@yandex.ru>
2025-07-23 19:51:40 +0300

    add init logic fix.

--- src/main/java/com/ctg/constants/PaginationConstants.java
+++ src/main/java/com/ctg/constants/PaginationConstants.java
@@ -3,6 +3,6 @@ package com.ctg.constants;
 public class PaginationConstants {
     public static final String DEFAULT_PAGE_NUMBER = "0";
     public static final String DEFAULT_PAGE_SIZE = "15";
-    public static final String DEFAULT_SORT_BY = "purchaseDate";
+    public static final String DEFAULT_SORT_BY = "id";
     public static final String DEFAULT_SORT_DIR = "asc";
 }
